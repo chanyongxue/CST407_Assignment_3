2221d2d87022cda0a5acd05d0aac11db7d38eeb6
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-04 22:17:40 -0700

    Create database_connection.dart

---
+++ lib/models/database_connection.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:jikan_api/jikan_api.dart';
+
+//class DatabaseConnection {
+  //final Top top;
+  //final bool isLiked;
+  //final DocumentReference reference;
+//}
