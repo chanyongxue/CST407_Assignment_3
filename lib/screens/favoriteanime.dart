5f0aa02e2b88ef3643b374037859321f48089b42
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-04 22:17:48 -0700

    Create favoriteanime.dart

---
+++ lib/screens/favoriteanime.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavoriteAnime extends StatelessWidget {
+  const FavoriteAnime({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+    );
+  }
+}
